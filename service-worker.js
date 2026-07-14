7a16c7cda00aeeb524c8638fac0fdf380015f86a
Sylvain Kuntzmann <13257444+skuntzmann@users.noreply.github.com>
2026-07-14 10:02:57 +0200

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "navigoupa-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./js/app.js",
+    "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+            .then(cache => cache.addAll(FILES))
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(response => response || fetch(event.request))
+
+    );
+
+});
